a089ca84c2a192b4c6eb8b99da6184429a61d120
thePPH <130406hungcg@gmail.com>
2026-04-03 17:41:42 +0800

    Task 2.1 code + trace output

---
+++ Lab2/part2/task2_1.pl
@@ -0,0 +1,72 @@
+% Royal Succession Rule - Old British Monarchy System
+% The throne passes along the male line according to birth order,
+% before consideration along the female line.
+
+% Family relationships
+offspring(elizabeth, charles, 1).
+offspring(elizabeth, anne, 2).
+offspring(elizabeth, andrew, 3).
+offspring(elizabeth, edward, 4).
+
+% Gender of each person
+male(charles).
+male(andrew).
+male(edward).
+male(william).
+male(henry).
+
+female(elizabeth).
+female(anne).
+
+% Determine if a person is male
+is_male(X) :- male(X).
+
+% Determine if a person is female
+is_female(X) :- female(X).
+
+% Get all offspring of a parent in birth order
+get_offspring(Parent, Child, BirthOrder) :-
+    offspring(Parent, Child, BirthOrder).
+
+% Get male offspring of a parent, sorted by birth order
+male_offspring(Parent, Child) :-
+    offspring(Parent, Child, _),
+    is_male(Child).
+
+% Get female offspring of a parent, sorted by birth order
+female_offspring(Parent, Child) :-
+    offspring(Parent, Child, _),
+    is_female(Child).
+
+% Succession rule: determine next in line for a given parent
+% First priority: male offspring in birth order
+% Second priority: female offspring in birth order
+successor(Parent, Successor) :-
+    offspring(Parent, Successor, BirthOrder1),
+    is_male(Successor),
+    \+ (offspring(Parent, Other, BirthOrder2),
+        is_male(Other),
+        BirthOrder2 < BirthOrder1).
+
+successor(Parent, Successor) :-
+    offspring(Parent, Successor, BirthOrder1),
+    is_female(Successor),
+    % Check no male offspring available
+    \+ (offspring(Parent, Male, _), is_male(Male)),
+    % Check no earlier female offspring
+    \+ (offspring(Parent, Other, BirthOrder2),
+        is_female(Other),
+        BirthOrder2 < BirthOrder1).
+
+% Succession from Elizabeth: returns entire line of succession as a list
+% Males first by birth order, then females by birth order
+line_of_succession(elizabeth, SuccessionList) :-
+    findall(BirthOrder-X, (offspring(elizabeth, X, BirthOrder), is_male(X)), MaleHeirs),
+    sort(MaleHeirs, SortedMales),
+    findall(BirthOrder-X, (offspring(elizabeth, X, BirthOrder), is_female(X)), FemaleHeirs),
+    sort(FemaleHeirs, SortedFemales),
+    pairs_values(SortedMales, Males),
+    pairs_values(SortedFemales, Females),
+    append(Males, Females, SuccessionList).
+
+
